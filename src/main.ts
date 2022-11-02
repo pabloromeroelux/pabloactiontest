6593b342344b3b07086741cd5603a6b5acd9394c
pabloromeroelux <pablo.romero-jaren@electrolux.com>
2022-11-02 17:03:56 +0100

    update input and output

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const key: string = core.getInput('key')
+    core.debug(`Waiting ...`)
+    if (key.length) {
+      core.setOutput('response', 'YES')
+    } else {
+      core.setOutput('response', 'NO')
+    }
   } catch (error) {
+    core.setOutput('response', 'NO')
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
