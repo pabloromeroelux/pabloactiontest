89b61d0104cd7eab5c4b69030c41044348073c71
pabloromeroelux <pablo.romero-jaren@electrolux.com>
2022-11-02 18:30:39 +0100

    supernova login

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import * as core from '@actions/core'
-// import {getData} from './supernova-login'
+import {getData} from './supernova-login'
 
 async function run(): Promise<void> {
   try {
     const key: string = core.getInput('key')
-    const first3 = key.slice(0, 3)
-    core.setOutput('response', first3)
-    // core.debug(`Waiting ...`)
-    // const workspaces = await getData(key)
-    // const workspace = workspaces[0]
-    // const designSystems = await workspace.designSystems()
-    // const designSystem = designSystems[0]
-    // const versions = await designSystem.versions()
-    // const version = versions[0]
-    // core.debug(`All good`)
-    // core.setOutput(
-    //   'response',
-    //   `DS: ${designSystem.name} with id ${designSystem.id}, Version: ${version.name} widh id ${version.id}`
-    // )
+    core.debug(`Waiting ...`)
+    const workspaces = await getData(key)
+    const workspace = workspaces[0]
+    const designSystems = await workspace.designSystems()
+    const designSystem = designSystems[0]
+    const versions = await designSystem.versions()
+    const version = versions[0]
+    core.debug(`All good`)
+    core.setOutput(
+      'response',
+      `DS: ${designSystem.name} with id ${designSystem.id}, Version: ${version.name} widh id ${version.id}`
+    )
   } catch (error) {
     core.setOutput('response', 'NO')
     if (error instanceof Error) core.setFailed(error.message)
